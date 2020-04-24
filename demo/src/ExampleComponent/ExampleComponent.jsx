577212328ef00c6b500e0560394005cbb40a2dbb
RyanLafferty <rlafferty1@hotmail.com>
2020-04-24 11:18:32 -0400

    Testing git hooks

--- demo/src/ExampleComponent/ExampleComponent.jsx
+++ demo/src/ExampleComponent/ExampleComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './ExampleComponent.module.scss';
 
 const ExampleComponent = () => (
-  <div className={styles.containers} />
+  <div className={styles.container} />
 );
 
 export default ExampleComponent;
