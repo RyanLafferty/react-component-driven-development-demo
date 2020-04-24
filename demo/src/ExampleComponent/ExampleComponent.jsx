1576067b2bcfeb3421c56b808460e2df1a516bf2
RyanLafferty <rlafferty1@hotmail.com>
2020-04-24 11:11:28 -0400

    Testing git precommit hooks

--- demo/src/ExampleComponent/ExampleComponent.jsx
+++ demo/src/ExampleComponent/ExampleComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './ExampleComponent.module.scss';
 
 const ExampleComponent = () => (
-  <div className={styles.container} />
+  <div className={styles.containers} />
 );
 
 export default ExampleComponent;
