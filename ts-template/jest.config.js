7033b9bfede55200870b29502c1a888e7929d9bc
RyanLafferty <rlafferty1@hotmail.com>
2020-06-15 14:50:34 -0400

    Remove typeroots

--- ts-template/jest.config.js
+++ ts-template/jest.config.js
@@ -12,4 +12,5 @@ module.exports = {
     "\\.module.(scss|css)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
   },
+  clearMocks: true,
 };
