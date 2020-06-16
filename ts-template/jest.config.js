3e7aa0491854a3c751d239efe75739c605517a66
RyanLafferty <rlafferty1@hotmail.com>
2020-06-15 21:50:48 -0400

    Removing the use of ts-jest as it is error prone

--- ts-template/jest.config.js
+++ ts-template/jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["node_modules/"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(js|ts)x?$": "babel-jest",
   },
   testMatch: ["**/*.test.(js|jsx|ts|tsx)"],
   moduleNameMapper: {
     "\\.(scss|css)$": "identity-obj-proxy",
-    "\\.module.(scss|css)$": "identity-obj-proxy",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
   },
   clearMocks: true,
