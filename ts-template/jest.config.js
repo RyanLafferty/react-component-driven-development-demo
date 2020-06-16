cd8645ef70cfe94ff9d3a7214fc193f4a9e2be8e
RyanLafferty <rlafferty1@hotmail.com>
2020-06-15 21:30:27 -0400

    Cleanup trying to figure out why jest isnt remapping scss modules.

--- ts-template/jest.config.js
+++ ts-template/jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  roots: ["./src"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testPathIgnorePatterns: ["node_modules/"],
   transform: {
     "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest",
   },
   testMatch: ["**/*.test.(js|jsx|ts|tsx)"],
   moduleNameMapper: {
     "\\.(scss|css)$": "identity-obj-proxy",
     "\\.module.(scss|css)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
   },
   clearMocks: true,
 };
